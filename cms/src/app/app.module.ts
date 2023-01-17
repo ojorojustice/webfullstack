db59374df8811beb82c93a2b3f0c0941055b9c56
ojo19001@byui.edu <ojo19001@byui.edu>
2023-01-17 05:15:06 +0100

    constructor method added to contact model class

--- cms/src/app/app.module.ts
+++ cms/src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { ContactDetailComponent } from './contact-detail/contact-detail.component';
-import { ContactListComponent } from './contact-list/contact-list.component';
+import { HeaderComponent } from './header.component';
+import { ContactDetailComponent } from './contacts/contact-detail/contact-detail.component';
+import { ContactListComponent } from './contacts/contact-list/contact-list.component';
+import { ContactsComponent } from './contacts/contacts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     ContactDetailComponent,
-    ContactListComponent
+    ContactListComponent,
+    ContactsComponent
   ],
   imports: [
     BrowserModule,
