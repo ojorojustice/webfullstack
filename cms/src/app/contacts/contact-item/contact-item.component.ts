85631b50635993f055c2dc7b0dc7bf3b8eba5ac5
ojo19001@byui.edu <ojo19001@byui.edu>
2023-01-22 07:07:42 +0100

    updates from Rufus

--- cms/src/app/contacts/contact-item/contact-item.component.ts
+++ cms/src/app/contacts/contact-item/contact-item.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Contact } from '../contact.model';
+
 
 @Component({
   selector: 'app-contact-item',
@@ -7,18 +8,11 @@ import { Contact } from '../contact.model';
   styleUrls: ['./contact-item.component.css']
 })
 export class ContactItemComponent implements OnInit{
-  contacts: Contact[]=[
-    new Contact(1,"R. Kent Jackson","jacksonk@byui.edu","208-496-3771","../../assets/images/jacksonk.jpg",[]),
-    new Contact(2,"Rex Barzee","barzeer@byui.edu","208-496-3768","../../assets/images/barzeer.jpg",[])
-  ];
+  @Input() contact!: Contact;
 
-constructor(){
+constructor(){}
 
-}
-
-
-ngOnInit(){
-  
+ngOnInit() {
 }
 
 }
--- cms/src/app/contacts/contact-list/contact-list.component.ts
+++ cms/src/app/contacts/contact-list/contact-list.component.ts
@@ -8,9 +8,19 @@ import { Contact } from '../contact.model';
 })
 export class ContactListComponent implements OnInit{
 
-  ngOnInit(){
+  contacts: Contact[]=[
+    new Contact(1,"R. Kent Jackson","jacksonk@byui.edu","208-496-3771","../../assets/images/jacksonk.jpg",[]),
+    new Contact(2,"Rex Barzee","barzeer@byui.edu","208-496-3768","../../assets/images/barzeer.jpg",[])
+  ];
+
+constructor(){
+
+}
+
+
+ngOnInit(){
   
-  }
+}
 
 }
 
