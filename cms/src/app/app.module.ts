1f5e99f56b1f80deaf97b1d0e9b1fb7071f27cc7
ojo19001@byui.edu <ojo19001@byui.edu>
2023-01-23 05:43:04 +0100

    message component added

--- cms/src/app/app.module.ts
+++ cms/src/app/app.module.ts
@@ -10,6 +10,15 @@ import { ContactDetailComponent } from './contacts/contact-detail/contact-detail
 import { ContactListComponent } from './contacts/contact-list/contact-list.component';
 import { ContactsComponent } from './contacts/contacts.component';
 import { ContactItemComponent } from './contacts/contact-item/contact-item.component';
+import { DocumentsComponent } from './documents/documents.component';
+import { DocumentListComponent } from './documents/document-list/document-list.component';
+import { DocumentItemComponent } from './documents/document-item/document-item.component';
+import { DocumentDetailComponent } from './documents/document-detail/document-detail.component';
+import { MessagesComponent } from './messages/messages.component';
+import { MessageItemComponent } from './messages/message-item/message-item.component';
+import { MessageListComponent } from './messages/message-list/message-list.component';
+import { MessageDetailComponent } from './messages/message-edit/message-detail.component';
+import { MessageEditComponent } from './messages/message-edit/message-edit.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +27,16 @@ import { ContactItemComponent } from './contacts/contact-item/contact-item.compo
     ContactDetailComponent,
     ContactListComponent,
     ContactsComponent,
-    ContactItemComponent
+    ContactItemComponent,
+    DocumentsComponent,
+    DocumentListComponent,
+    DocumentItemComponent,
+    DocumentDetailComponent,
+    MessagesComponent,
+    MessageItemComponent,
+    MessageListComponent,
+    MessageDetailComponent,
+    MessageEditComponent
   ],
   imports: [
     BrowserModule,
