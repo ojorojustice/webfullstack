4067dd82bf7b4fcdd5de77c89724634bb4ad5582
ojo19001@byui.edu <ojo19001@byui.edu>
2023-02-14 05:28:57 +0100

    Pushing for week 6

--- cms/src/app/app-routing.module.ts
+++ cms/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactsComponent } from './contacts/contacts.component';
+import { DocumentsComponent } from './documents/documents.component';
+import { MessagesComponent } from './messages/messages.component';
 
-const routes: Routes = [];
+const appRoutes: Routes = [
+  {path:'', redirectTo:'/documents'},
+  {path:'documents',component:DocumentsComponent},
+  {path:'messages',component: MessagesComponent},
+  {path:'contacts',component:ContactsComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
